43b91d91e23e0a4bfd04b256138f08f1f2b07ea4
Mubens <zhuxiaofengzj@qq.com>
2020-08-07 14:32:40 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,1 @@
-import './css/style.less'
-
-const p: Promise<string> = new Promise((resovle, reject) => {
-  setTimeout(() => {
-    resovle('ok')
-  }, 1000)
-})
-
-p.then((res) => {
-  console.log(res)
-}).catch((err) => {
-  console.error(err)
-})
+import './less/style.less'
